c87c73a6c1d493790afadaed098722b874c288c9
Ulysse <ulysse.gresselugue@gmail.com>
2025-03-13 10:15:10 +0100

    first commit

---
+++ app/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fixed top-0 left-0 w-screen z-50">
+    <div class="relative overflow-hidden outline-gray-600 outline-2">
+      <div class="bg-black opacity-25 blur-lg w-screen h-full absolute inset-0"></div>
+      <nav class="flex items-center justify-between relative z-10 p-5">
+        <img alt="Logo" src="assets/logo.svg" draggable="false" class="w-md" />
+        <div class="flex items-center gap-14">
+        </div>
+      </nav>
+    </div>
+  </div>
+
+  <div class="pt-20">
+    <UApp>
+      <NuxtPage />
+    </UApp>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="m-40">
+    <div class="grid grid-cols-1 grid-rows-4 items-center gap-2 mt-35 content-center">
+      <div class="grid grid-cols-2 grid-rows-1 gap-20">
+        <p>
+          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
+          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
+          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
+          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
+        </p>
+        <img alt="Logo" src="assets/devanturelycee.png" draggable="false" class="w-md float-right rounded-xl" />
+      </div>
+      <hr/>
+      <div class="grid grid-cols-2 grid-rows-1 gap-20 content-center">
+        <img alt="Logo" src="assets/devanturelycee.png" draggable="false" class="w-md float-left rounded-xl" />
+        <p>
+          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
