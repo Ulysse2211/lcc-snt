73602dc2bc9aa51ba350f18677e44e49c20a7831
Ulysse <ulysse.gresselugue@gmail.com>
2025-03-27 10:19:29 +0100

    edited text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,12 +2,14 @@
   <div class="m-40">
     <div class="grid grid-cols-1 grid-rows-4 items-center gap-2 mt-35 content-center">
       <div class="grid grid-cols-2 grid-rows-1 gap-20">
-        <p>
-          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
-          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
-          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
-          Bla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla BlaBla Bla
-        </p>
+        <p>Le lycée est composé de cinq bâtiments où se déroulent les cours. Le premier compte quatre étages, le deuxième en a trois, tout comme le troisième et le quatrième, tandis que le dernier ne comporte que deux étages.
+          <br><br>
+          Au sein de ces bâtiments, vous trouverez également le CDI, la cantine, l'infirmerie, les bureaux administratifs, ainsi que la vie scolaire.
+          <br><br>
+          Le lycée Camille Claudel propose différentes filières :<br>
+          - Filière générale, avec les spécialités : HGGSP, Mathématiques, Physique-Chimie, HLP, SES, NSI, Arts, Musique, SVT, etc.<br>
+          - Filière technologique: STI2D, STMG, STI2A.<br>
+          - Filière professionnelle : MARVI.</p>
         <img alt="Logo" src="assets/devanturelycee.png" draggable="false" class="w-md float-right rounded-xl" />
       </div>
       <hr/>
